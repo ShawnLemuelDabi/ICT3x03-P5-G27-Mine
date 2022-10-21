e0045083cd7665760504e2deefac0a9120bcc8ee
FL Chong <fungloonchong@gmail.com>
2022-10-21 15:57:11 +0800

    Updated MFA implementation
    - Added `verify_otp_from_secret`, `generate_recovery_code`, `generate_recovery_codes`, `confirm_mfa_enabled` to `mfa.py`
    - Updated the implementation of MFA login
    - Added `Recovery_Codes` model
    - Added basic view for enabling mfa
    - Added ignore word for cSpell

--- flasks/user.py
+++ flasks/user.py
@@ -31,6 +31,8 @@ class User(UserMixin, db.Model):
 
     bookings = db.relationship("Booking", back_populates="user")
 
+    recovery_codes = db.relationship("Recovery_Codes", back_populates="user")
+
     def __repr__(self):
         return f"User(user_id={self.user_id!r}, email={self.email!r}, first_name={self.first_name!r}, last_name={self.last_name!r}, password={self.password!r}, phone_number={self.phone_number!r}, license_blob={self.license_blob!r}, license_filename={self.license_filename!r}, license_mime={self.license_mime!r}, mfa_secret={self.mfa_secret!r}, role={self.role!r})"
 
