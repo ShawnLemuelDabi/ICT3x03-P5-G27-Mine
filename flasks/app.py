bbfe64bd6bbfe0e62dab30bca0b52e6460c6c1d3
FL Chong <fungloonchong@gmail.com>
2022-11-05 11:05:38 +0800

    Same bug as the previous commit

--- flasks/app.py
+++ flasks/app.py
@@ -353,7 +353,7 @@ def register_verified(token: str) -> str:
         except Exception as e:
             err_handler.push(
                 user_message="Invalid token",
-                log_message=f"Invalid token. {e}. Email attempted: {email}"
+                log_message=f"Invalid token. {e}"
             )
 
         err_handler.commit_log()
